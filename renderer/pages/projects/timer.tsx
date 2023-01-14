beafe4f5df46bd383deb3236bd9846048afc4f2b
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-01-14 21:28:55 +0100

    timer not workign

--- renderer/pages/projects/timer.tsx
+++ renderer/pages/projects/timer.tsx
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Head from 'next/head';
-import './components/navbar';
-import Navbar from './components/navbar';
+import Navbar from '../components/navbar';
 function Home() {
+  let t: number;
+  const [time, setTime] = useState(0);
+  const [run, setRun] = useState(false);
+
+  function startTimer() {
+    const i = setInterval(addToTimer, 1000);
+
+    if (run === true) {
+      clearInterval(i);
+      return setRun(false);
+    }
+    setRun(true);
+  }
+  function addToTimer() {
+    console.log(t);
+    t = t + 1;
+    setTime(t);
+  }
   return (
     <>
       <Head>
-        <title>Home</title>
+        <title>Timer</title>
       </Head>
-      <div>
-        <Navbar />
-        <a></a>
-      </div>
+      <main>
+        <div className="flex flex-col items-center justify-center">
+          <Navbar />
+          <a>{time}</a>
+          <button onClick={startTimer}>{run ? 'stop' : 'start'}</button>
+        </div>
+      </main>
     </>
   );
 }
