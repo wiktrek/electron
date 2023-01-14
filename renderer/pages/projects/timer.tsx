fc4fc28a05cb9cca1f8b1c1566bedcd011cc8161
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-01-14 21:54:30 +0100

    timer works

--- renderer/pages/projects/timer.tsx
+++ renderer/pages/projects/timer.tsx
@@ -1,25 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Head from 'next/head';
 import Navbar from '../components/navbar';
 function Home() {
-  let t: number;
   const [time, setTime] = useState(0);
   const [run, setRun] = useState(false);
-
+  const [timer, setTimer] = useState<NodeJS.Timer>();
+  let t: number = 0;
   function startTimer() {
     const i = setInterval(addToTimer, 1000);
-
-    if (run === true) {
-      clearInterval(i);
-      return setRun(false);
-    }
+    setTimer(i);
     setRun(true);
   }
+  function stopTimer() {
+    clearInterval(timer);
+    setTime(0);
+    return setRun(false);
+  }
   function addToTimer() {
-    console.log(t);
+    console.log(run);
     t = t + 1;
+    console.log(t);
     setTime(t);
   }
+
   return (
     <>
       <Head>
@@ -28,8 +31,13 @@ function Home() {
       <main>
         <div className="flex flex-col items-center justify-center">
           <Navbar />
-          <a>{time}</a>
-          <button onClick={startTimer}>{run ? 'stop' : 'start'}</button>
+          <a>{time.toFixed()}</a>
+          <button
+            onClick={run ? stopTimer : startTimer}
+            className=" h-20 w-20 bg-slate-400 rounded-xl text-2xl text-black"
+          >
+            {run ? 'stop' : 'start'}
+          </button>
         </div>
       </main>
     </>
